d6aea151302d34e9120e842db2e9350285b8379d
Derek Douglas <derek.j.douglas13@gmail.com>
2022-06-15 22:22:06 -0500

    adds test endpoint

--- server.js
+++ server.js
@@ -18,4 +18,6 @@ db.once('open', () => console.log('Mongoose is connected'));
 
 const PORT = process.env.PORT || 3001;
 
+app.get('/test', (request, response) => response.send('test request received'));
+
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
