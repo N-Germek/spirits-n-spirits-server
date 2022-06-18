f9e36b3828d07cdc6cbc7f35b27544b2a9c60ac1
Derek Douglas <derek.j.douglas13@gmail.com>
2022-06-18 12:09:05 -0500

    continues building api calls

--- modules/tarot.js
+++ modules/tarot.js
@@ -1,21 +1,29 @@
 'use strict';
+const axios = require('axios');
 const CardData = require('../card_data.json');
 const Tarot = {};
 
-Tarot.draw = (request, response, next) => {
+Tarot.draw = async (request, response, next) => {
   try {
     let major = CardData.filter(value => value.type === 'major');
     let drawnCard = major[Math.floor(Math.random() * major.length)];
     if (drawnCard.name === 'Wheel Of Fortune') {
       //use ternary for alcohol/non-alcoholic
       //choose random drink ingredient
+      //Pass alcohol/non-a in request
+
     } else if (drawnCard.name === 'Death') {
       //use ternary for alcohol/non-alcoholic
       //search user history for drink never tried
       //if fails, choose random drink ingredient
     } else {
       //use ternary for alcohol/non-alcoholic
+      let drinkTarget = request.body.drinkTarget === 'alcoholic' ? drawnCard.drinks.alcoholic : drawnCard.drinks['non-alcoholic'];
       //call drink api
+      //Remove async functionality?
+      let drink = await axios.get(`${process.env.COCKTAILDB_URL}${drinkTarget}`);
+      //May need to package vars in object
+      response.status(200).send(drawnCard, drink);
     }
     response.status(200).send(drawnCard);
   } catch (error) {
