a581209de4024ed057f655b6a72d8001d9d55f7d
Aiden <hyena.shenanigans@gmail.com>
2022-06-21 20:56:51 -0700

    Changed console.logs, fixed data accessing to query

--- modules/tarot.js
+++ modules/tarot.js
@@ -6,17 +6,17 @@ const Tarot = {};
 let apiURL;
 
 Tarot.draw = async (request, response, next) => {
+  console.log(request)
   try {
     let major = CardData.cards.filter(value => value.type === 'major');
     let drawnCard = major[Math.floor(Math.random() * major.length)];
     console.log(drawnCard.name);
     if (drawnCard.name === 'Wheel Of Fortune') {
       //use ternary for alcohol/non-alcoholic
-      let dbTarget = request.params.drinkTarget === 'alcoholic' ? 'Alcoholic' : 'Non_Alcoholic';
+      let dbTarget = request.query.drinkTarget === 'alcoholic' ? 'Alcoholic' : 'Non_Alcoholic';
       apiURL = `${process.env.COCKTAILDB_URL}filter.php?a=${encodeURIComponent(dbTarget)}`;
       let filteredDrinks = await axios.get(apiURL);
       let banana = filteredDrinks.data.drinks;
-      console.log(banana, 'Wheel');
       let drinkTarget = banana[Math.floor(Math.random() * banana.length)].strDrink;
       const apiURL2 = `${process.env.COCKTAILDB_URL}search.php?s=${encodeURIComponent(drinkTarget)}`;
       let drink = await axios.get(apiURL2);
@@ -25,7 +25,7 @@ Tarot.draw = async (request, response, next) => {
       //Pass alcohol/non-a in request
 
     } else if (drawnCard.name === 'Death') {
-      let dbTarget = request.body.drinkTarget === 'alcoholic' ? 'Alcoholic' : 'Non_Alcoholic';
+      let dbTarget = request.query.drinkTarget === 'alcoholic' ? 'Alcoholic' : 'Non_Alcoholic';
       apiURL = `${process.env.COCKTAILDB_URL}filter.php?a=${encodeURIComponent(dbTarget)}`;
       let filteredDrinks = await axios.get(apiURL);
       let banana = filteredDrinks.data.drinks;
@@ -38,13 +38,12 @@ Tarot.draw = async (request, response, next) => {
       //if fails, choose random drink ingredient
     } else {
       //use ternary for alcohol/non-alcoholic
-      let drinkTarget = request.body.drinkTarget === 'alcoholic' ? drawnCard.drinks.alcoholic : drawnCard.drinks['non-alcoholic'];
+      console.log(request.query.drinkTarget, 'query');
+      let drinkTarget = request.query.drinkTarget === 'alcoholic' ? drawnCard.drinks.alcoholic : drawnCard.drinks['non-alcoholic'];
       //call drink api
       //Remove async functionality?
       apiURL = `${process.env.COCKTAILDB_URL}search.php?s=${encodeURIComponent(drinkTarget)}`;
-      console.log(apiURL);
       let drink = await axios.get(apiURL);
-      console.log(drink.data, 'Else');
       //May need to package vars in object
       //
       response.status(200).send({drawnCard: drawnCard, drinkChosen: drink.data});
